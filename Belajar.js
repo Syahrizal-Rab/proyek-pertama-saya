a1f3968b8e6a0b368fffa58b74024f9c275443a8
Syahrizal-Rab <syahrizalekasaputra@gmail.com>
2026-03-30 22:54:18 +0700

    Update Profil pertama saya

---
+++ Belajar.js
@@ -0,0 +1,29 @@
+const namaLaptop = "Laptop baru semangat baru";
+let mood = "Siap belajar";
+
+console.log("===== Test Belajar =====")
+console.log("Status : " + namaLaptop);
+console.log("Kondisi Rizal : " + mood);
+
+//1. Tipe Data STRING (Teks) - Pakai tanda kutip
+const nama = "Syahrizal";
+
+//2. Tipe Data NUMBER (Ankka) - Tanpa tanda kutip
+let Umur = 23;
+
+//3. Tipe Data BOOLEAN (Logika) - Cuma ada True & False
+const sedangBelajar = true;
+
+//4. Tipe Data NULL (Kosong) - Artinya nilainya sengaja dikosongin
+let pacar = null;
+
+//5. Tipe Data Undifined - Variabelnya udah di buat tapi gak ada isinya 
+let tabungan;
+
+// Menampilkan Hasil
+console.log(" === Profil ===");
+console.log("Nama : " + nama);
+console.log("Umur : " + Umur);
+console.log("Status Belajar : " + sedangBelajar);
+console.log("Status Pacar : " + pacar);
+console.log("Isi Tabungan : " + tabungan);
